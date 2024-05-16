b9cde7deea91b6667b78788ef07bca73ae31aea3
Jarrod Swords <jarrod.swords@gmail.com>
2024-05-16 16:47:34 -0400

    add messagetype to message

--- store-spec/source/WhenCreatingAMessage.cs
+++ store-spec/source/WhenCreatingAMessage.cs
@@ -7,9 +7,18 @@ public class WhenCreatingAMessage
     [Fact]
     public void ThenIdIsGenerated()
     {
-        var message = new Message();
+        var message = new Message(MessageType.Create("FooCreated"));
 
         message.Id.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public void ThenMessageTypeIsSet()
+    {
+        MessageType type = MessageType.Create("FooCreated");
+        var message = new Message(type);
+
+        message.Type.Should().Be(type);
     }
 
     #endregion
--- store/source/Message.cs
+++ store/source/Message.cs
@@ -2,5 +2,11 @@
 
 public record Message
 {
+    public Message(MessageType type)
+    {
+        Type = type;
+    }
+
     public Guid Id { get; } = NewGuid();
+    public MessageType Type { get; }
 }
