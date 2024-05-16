c63445415bffedbf9cb9e4e7d61d6fe9e38bfc63
Jarrod Swords <jarrod.swords@gmail.com>
2024-05-16 14:30:43 -0400

    add message type

---
+++ store-spec/source/WhenCreatingAMessageType.cs
@@ -0,0 +1,19 @@
+﻿namespace Store.Spec;
+
+public class WhenCreatingAMessageType
+{
+    #region Requirements
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void WithoutName_ThenReturnNameRequiredError(string value)
+    {
+        var error = MessageType.Create(value).Error;
+
+        error.Should().Be(MessageType.NameRequired());
+    }
+
+    #endregion
+}
--- store/source/Usings.cs
+++ store/source/Usings.cs
@@ -1,1 +1,4 @@
-﻿global using static System.Guid;
+﻿global using Jgs.Errors;
+global using Jgs.Errors.Results;
+global using Jgs.ValueObjects;
+global using static System.Guid;
