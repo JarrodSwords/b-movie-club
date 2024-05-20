4b497a92e9c4fc137e60a4bbeaec953d35f897f6
Jarrod Swords <jarrod.swords@gmail.com>
2024-05-20 12:47:47 -0400

    add infrastructure

---
+++ Store.Infrastructure.Dapper.Spec/source/WhenConvertingToInfrastructure.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using Store.Domain;
+using DomainMessage = Store.Domain.Message;
+
+namespace Store.Infrastructure.Dapper.Spec;
+
+public class WhenConvertingToInfrastructure
+{
+    #region Requirements
+
+    [Fact]
+    public void ThenFieldsAreExpected()
+    {
+        var source = new DomainMessage(MessageType.Create("FooCreated"));
+
+        Message message = source;
+
+        using var scope = new AssertionScope();
+
+        message.Id.Should().Be(source.Id);
+        message.Type.Should().Be(source.Type);
+    }
+
+    #endregion
+}
---
+++ Store.Infrastructure.Dapper/source/Message.cs
@@ -0,0 +1,8 @@
+﻿using DomainMessage = Store.Domain.Message;
+
+namespace Store.Infrastructure.Dapper;
+
+public record Message(Guid Id, string Type)
+{
+    public static implicit operator Message(DomainMessage source) => new(source.Id, source.Type);
+}
