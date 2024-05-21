800f5fa11e0d141281a581c9b02f686a8a9e5512
Jarrod Swords <jarrod.swords@gmail.com>
2024-05-21 01:25:40 -0400

    messageid

---
+++ store-domain-spec/source/WhenCreatingAMessageId.cs
@@ -0,0 +1,36 @@
+﻿namespace Store.Domain.Spec;
+
+public class WhenCreatingAMessageId
+{
+    #region Requirements
+
+    [Fact]
+    public void FromGuid_ThenValueIsExpected()
+    {
+        var source = Guid.NewGuid();
+
+        MessageId messageId = source;
+
+        messageId.Value.Should().Be(source);
+    }
+
+    [Fact]
+    public void ThenValueIsNotEmpty()
+    {
+        var messageId = new MessageId();
+
+        messageId.Value.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public void WithGuid_ThenValueIsExpected()
+    {
+        var source = Guid.NewGuid();
+
+        var messageId = new MessageId(source);
+
+        messageId.Value.Should().Be(source);
+    }
+
+    #endregion
+}
---
+++ store-domain/source/MessageId.cs
@@ -0,0 +1,14 @@
+﻿namespace Store.Domain;
+
+public class MessageId : TinyType<Guid>
+{
+    public MessageId() : this(NewGuid())
+    {
+    }
+
+    public MessageId(Guid value) : base(value)
+    {
+    }
+
+    public static implicit operator MessageId(Guid source) => new(source);
+}
