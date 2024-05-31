a6c35d0b0fa22e933c4df3fdc9a9f078d09ecaf8
Jarrod Swords <jarrod.swords@gmail.com>
2024-05-31 14:59:08 -0400

    add original position

--- store-domain-spec/source/Store/when-pushing-a-message/PositionGeneration.cs
+++ store-domain-spec/source/Store/when-pushing-a-message/PositionGeneration.cs
@@ -1,12 +1,29 @@
-﻿namespace Store.Domain.Spec.Store;
+﻿using FluentAssertions.Execution;
+
+namespace Store.Domain.Spec.Store;
 
 public abstract partial class WhenPushingAMessage
 {
-    //[Fact]
-    //public void GivenANewEntity_ThenPositionIs0()
-    //{
-    //    throw new NotImplementedException();
-    //}
+    #region Requirements
+
+    [Fact]
+    public void GivenANewEntity_ThenPositionIs0()
+    {
+        var createFoo1 = new CandidateMessage(new CreateFoo(), 0);
+        var createFoo2 = new CandidateMessage(new CreateFoo(), 0);
+
+        _store.Push(createFoo1);
+        _store.Push(createFoo2);
+
+        var message1 = _store.Find(createFoo1.MessageId).Value!;
+        var message2 = _store.Find(createFoo2.MessageId).Value!;
+
+        using var scope = new AssertionScope();
+        message1.Metadata.Position.Should().Be(0u);
+        message2.Metadata.Position.Should().Be(0u);
+    }
+
+    #endregion
 
     //[Fact]
     //public void GivenAnExistingEntity_ThenPositionIsPreviousMaxPlus1()
